54b2576d145ea9f505fbd8689fdff799ff0899db
Alfath Noorislami H <alfathnoor11@gmail.com>
2025-09-10 15:45:14 +0700

    Add files via upload

--- Latihan 4(Tugas).php
+++ Latihan 4(Tugas).php
@@ -1,6 +1,7 @@
 <?php
 // Membuat class BangunRuang
-class BangunRuang {
+class BangunRuang
+{
     private $jenis;
     private $sisi;
     private $jari;
@@ -8,51 +9,59 @@ class BangunRuang {
     private $volume;
 
     // Setter dan getter
-    public function setJenis($jenis) {
+    public function setJenis($jenis)
+    {
         $this->jenis = $jenis;
     }
 
-    public function getJenis() {
+    public function getJenis()
+    {
         return $this->jenis;
     }
 
-    public function setSisi($sisi) {
+    public function setSisi($sisi)
+    {
         $this->sisi = $sisi;
     }
 
-    public function getSisi() {
+    public function getSisi()
+    {
         return $this->sisi;
     }
 
-    public function setJari($jari) {
+    public function setJari($jari)
+    {
         $this->jari = $jari;
     }
 
-    public function getJari() {
+    public function getJari()
+    {
         return $this->jari;
     }
 
-    public function setTinggi($tinggi) {
+    public function setTinggi($tinggi)
+    {
         $this->tinggi = $tinggi;
     }
 
-    public function getTinggi() {
+    public function getTinggi()
+    {
         return $this->tinggi;
     }
 
-    public function getVolume() {
+    public function getVolume()
+    {
         return $this->volume;
     }
 
-public function hitungVolume() {
-        switch($this->jenis) {
+    public function hitungVolume()
+    {
+        switch ($this->jenis) {
             case "Bola":
-                // Mengganti pi() dengan 3.14 agar sesuai soal
-                $this->volume = (4/3) * 3.14 * pow($this->jari, 3);
+                $this->volume = (4 / 3) * 3.14 * pow($this->jari, 3);
                 break;
             case "Kerucut":
-                // Mengganti pi() dengan 3.14 agar sesuai soal
-                $this->volume = (1/3) * 3.14 * pow($this->jari, 2) * $this->tinggi;
+                $this->volume = (1 / 3) * 3.14 * pow($this->jari, 2) * $this->tinggi;
                 break;
             case "Limas Segi Empat":
                 $this->volume = pow($this->sisi, 2) * $this->tinggi / 3;
@@ -61,7 +70,6 @@ public function hitungVolume() {
                 $this->volume = pow($this->sisi, 3);
                 break;
             case "Tabung":
-                // Mengganti pi() dengan 3.14 agar sesuai soal
                 $this->volume = 3.14 * pow($this->jari, 2) * $this->tinggi;
                 break;
             default:
@@ -92,19 +100,19 @@ echo "<tr style='background-color: blue; color: white;'>
 
 // Perulangan untuk setiap data bangun ruang
 foreach ($bangunRuang as $data) {
-    $br = new BangunRuang();
-    $br->setJenis($data['jenis']);
-    $br->setSisi($data['sisi']);
-    $br->setJari($data['jari']);
-    $br->setTinggi($data['tinggi']);
-    $br->hitungVolume();
+    $new = new BangunRuang();
+    $new->setJenis($data['jenis']);
+    $new->setSisi($data['sisi']);
+    $new->setJari($data['jari']);
+    $new->setTinggi($data['tinggi']);
+    $new->hitungVolume();
 
     echo "<tr>";
-    echo "<td>" . $br->getJenis() . "</td>";
-    echo "<td>" . $br->getSisi() . "</td>";
-    echo "<td>" . $br->getJari() . "</td>";
-    echo "<td>" . $br->getTinggi() . "</td>";
-    echo "<td>" . $br->getVolume() . "</td>";
+    echo "<td>" . $new->getJenis() . "</td>";
+    echo "<td>" . $new->getSisi() . "</td>";
+    echo "<td>" . $new->getJari() . "</td>";
+    echo "<td>" . $new->getTinggi() . "</td>";
+    echo "<td>" . $new->getVolume() . "</td>";
     echo "</tr>";
 }
 ?>
